32db63cb85b5f48e736e0e0f8178fe63c2d2258f
xuantruong161099 <phanxuantruong16@gmail.com>
2021-05-06 10:59:57 +0700

    add some func

--- EG project/Assets/Script/Gun.cs
+++ EG project/Assets/Script/Gun.cs
@@ -5,12 +5,15 @@ using UnityEngine.EventSystems;
 
 public class Gun : MonoBehaviour
 {
+    public Game_Manager game_Manager;
     public bool inUsed = false;
     public GameObject projectile;
     public float bulletSpeed;
     public Transform firePoint;
     Rigidbody2D rb2d;
-    public float rotateSpeed;
+    //float speedRotate = 5f;
+    public float fireRate = 0.1f;
+    private float nextFire = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +23,9 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(inUsed && Input.touchCount > 0)
+        if(game_Manager.playerIn == this && Input.touchCount > 0 )
         {
-            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            if (Input.GetTouch(0).phase == TouchPhase.Began || Input.GetTouch(0).phase == TouchPhase.Stationary || Input.GetTouch(0).phase == TouchPhase.Moved)
             {
                 if (EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
                 {
@@ -30,15 +33,20 @@ public class Gun : MonoBehaviour
                 }
                 else
                 {
-                    Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
-                    Vector2 lookdir = touchPos - rb2d.position;
-                    float angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
-                    rb2d.rotation = angle;
+                    if(Time.time > nextFire)
+                    {
+                        nextFire = Time.time + fireRate;
+                        Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                        Vector2 lookdir = touchPos - rb2d.position;
+                        float angle = Mathf.Atan2(lookdir.y, lookdir.x) * Mathf.Rad2Deg - 90f;
+                        rb2d.rotation = angle;
 
-                    Vector2 firedir = touchPos - (new Vector2(firePoint.position.x, firePoint.position.y));
-                    firedir.Normalize();
-                    GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
-                    bullet.GetComponent<Rigidbody2D>().velocity = firedir * bulletSpeed;
+                        Vector2 firedir = touchPos - (new Vector2(firePoint.position.x, firePoint.position.y));
+                        firedir.Normalize();
+                        GameObject bullet = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+                        bullet.GetComponent<Rigidbody2D>().velocity = firedir * bulletSpeed;
+                    }    
+
                 }
             }
             else if (Input.GetTouch(0).phase == TouchPhase.Stationary)
--- EG project/Assets/Script/SwitchGun.cs
+++ EG project/Assets/Script/SwitchGun.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class SwitchGun : MonoBehaviour
 {
     // Start is called before the first frame update
     public Gun[] slotGun;
+    public Button[] btn;
     public Game_Manager game_Manager;
     void Start()
     {
@@ -15,7 +16,15 @@ public class SwitchGun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < slotGun.Length; i++)
+        {
+            if (slotGun[i].inUsed)
+            {
+                btn[i].interactable = false;
+            }
+            else
+                btn[i].interactable = true;
+        }
     }
 
     public void switchGun(int i)
